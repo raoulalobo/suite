e93488ed160bbb47a6bcad52e8773d50b65a70b9
raoulalobo <nathanaelalob@gmail.com>
2020-07-15 12:38:36 +0100

    Ajout export Colis

--- colis_apps/admin.py
+++ colis_apps/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from .models import Coli, Profile, Client , Retour
-
+from import_export.admin import ImportExportModelAdmin
 # Register your models here.
 
 @admin.register(Client)
@@ -22,8 +22,7 @@ class ProfileAdmin(admin.ModelAdmin):
 
 
 @admin.register(Coli)
-class PlainteAdmin(admin.ModelAdmin):
-    list_display = ['dateheure', 'numero_colis', 'etat_colis', 'telephone_exp', 'telephone_dest', 'destination']
+class ColiAdmin(ImportExportModelAdmin):
+    change_list_filter_template = "admin/filter_listing.html"
+    list_display = ['dateheure', 'numero_colis', 'nom_exp' , 'telephone_exp', 'nom_dest' , 'telephone_dest', 'destination' ,'valeur_declaree', 'montant' , 'libelle' , 'immatriculation', 'etat_colis']
     list_filter = ['numero_colis', 'telephone_exp', 'telephone_dest']
-
-#admin.site.register( DataBase )
